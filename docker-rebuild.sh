30279111aee306e8d791ba6f54d86f2ae058e761
Dani Roxberry <dani@bitoriented.com>
2018-05-25 19:34:46 -0700

    [fix] rm cache on rebuild

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -2,4 +2,5 @@ docker-compose down && \
 docker volume rm docker-django-react_pgdata && \
 docker volume rm docker-django-react_frontend && \
 docker volume rm docker-django-react_backend && \
+rm -rf frontend/cache && \
 docker-compose up --build --quiet-pull --abort-on-container-exit
